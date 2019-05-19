cb459def5a835a2db5aace6d72e00014eb5448f6
Hector Flores <frhectorin@gmail.com>
2019-05-19 16:03:44 -0500

    .gitignore is now working

--- config.js
+++
@@ -1,8 +0,0 @@
-const user = {
-    usuario: "cip_user", // Puede ser cualquier cosa
-    password: "1234", // Puede ser cualquier cosa
-    afiliacion: "org1", // Puede ser org1, org2, org1.department1, org1.department2, org2.department1
-};
-
-
-module.exports = user;
