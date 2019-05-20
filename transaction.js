b622db672f906e08129c59c25352f5b1e674c566
Hector Flores <frhectorin@gmail.com>
2019-05-20 17:51:29 -0500

    Changed Blockchain

--- transaction.js
+++ transaction.js
@@ -4,22 +4,38 @@ module.exports = {
     chaincodeID: "deviceid",
     channelID: "scd-deviceid",
     peers: [
-        {url: "grpcs://peer1:7051", certificadoPem: "-----BEGIN CERTIFICATE-----\nMIICUjCCAfmgAwIBAgIQEb//gedPyqfbZdUsdtbKJDAKBggqhkjOPQQDAjB0MQsw\nCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZy\nYW5jaXNjbzEYMBYGA1UEChMPaGV4dC5zY2Qub3JnLmJyMR4wHAYDVQQDExV0bHNj\nYS5oZXh0LnNjZC5vcmcuYnIwHhcNMTkwNTE5MTgzNzAwWhcNMjkwNTE2MTgzNzAw\nWjB0MQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMN\nU2FuIEZyYW5jaXNjbzEYMBYGA1UEChMPaGV4dC5zY2Qub3JnLmJyMR4wHAYDVQQD\nExV0bHNjYS5oZXh0LnNjZC5vcmcuYnIwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNC\nAAQC5bYs8zzRSRNLKaDdGrGZMGvGDWATnB3g9etcWF2AkaHubOMyAukqD5g390SU\nLMvx9jO82GtbOq0J2AHlhel6o20wazAOBgNVHQ8BAf8EBAMCAaYwHQYDVR0lBBYw\nFAYIKwYBBQUHAwIGCCsGAQUFBwMBMA8GA1UdEwEB/wQFMAMBAf8wKQYDVR0OBCIE\nIASnuDit2VEVKTcHnHQoZ22y0Q8X/oIwBmNl+Idm4iWhMAoGCCqGSM49BAMCA0cA\nMEQCIFYGtwRXfxdRVd+8SlMPOgU3ypOdjKFKZ5Y2cdbYrnANAiBagkx+apIfM6B6\nTvR9c3L1PVNAMzYBzPOxek3eoDfV0Q==\n-----END CERTIFICATE-----"},
+        {url: "grpcs://peer1:7051", certificadoPem: "-----BEGIN CERTIFICATE-----\n" +
+                "MIICejCCAiCgAwIBAgIRAKVq2Bmi9xCuXOObXnRRfm8wCgYIKoZIzj0EAwIwgYYx\n" +
+                "CzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYDVQQHEw1TYW4g\n" +
+                "RnJhbmNpc2NvMSEwHwYDVQQKExgwNDM5MTAwNy5oZXh0LnNjZC5vcmcuYnIxJzAl\n" +
+                "BgNVBAMTHnRsc2NhLjA0MzkxMDA3LmhleHQuc2NkLm9yZy5icjAeFw0xOTA1MjAx\n" +
+                "OTQ2MDBaFw0yOTA1MTcxOTQ2MDBaMIGGMQswCQYDVQQGEwJVUzETMBEGA1UECBMK\n" +
+                "Q2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZyYW5jaXNjbzEhMB8GA1UEChMYMDQz\n" +
+                "OTEwMDcuaGV4dC5zY2Qub3JnLmJyMScwJQYDVQQDEx50bHNjYS4wNDM5MTAwNy5o\n" +
+                "ZXh0LnNjZC5vcmcuYnIwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAAST4MDVvLPV\n" +
+                "iFp9bdQBZwiKyHTviI6/d48Dm2/UPC7vOxPFsR/Ll98DAHxY2ET08PiwP0c5SD5W\n" +
+                "UYaesdkryMgro20wazAOBgNVHQ8BAf8EBAMCAaYwHQYDVR0lBBYwFAYIKwYBBQUH\n" +
+                "AwIGCCsGAQUFBwMBMA8GA1UdEwEB/wQFMAMBAf8wKQYDVR0OBCIEILDG9uilqE/f\n" +
+                "VVrBUT46E4yNut5AmQkCVAzcQ49HXz1mMAoGCCqGSM49BAMCA0gAMEUCIQCyBSdu\n" +
+                "MlNdxZvQSfdoBjUtkA0Xy3iQJErhhdJo1cDzAwIgHz4VhX8MOfRPz1FpROttKqqR\n" +
+                "6IwjzVakqiRbqt9wzBg=\n" +
+                "-----END CERTIFICATE-----"},
         {url: "grpcs://peer2:7051", certificadoPem: "-----BEGIN CERTIFICATE-----\n" +
-                "MIICUjCCAfmgAwIBAgIQEb//gedPyqfbZdUsdtbKJDAKBggqhkjOPQQDAjB0MQsw\n" +
-                "CQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZy\n" +
-                "YW5jaXNjbzEYMBYGA1UEChMPaGV4dC5zY2Qub3JnLmJyMR4wHAYDVQQDExV0bHNj\n" +
-                "YS5oZXh0LnNjZC5vcmcuYnIwHhcNMTkwNTE5MTgzNzAwWhcNMjkwNTE2MTgzNzAw\n" +
-                "WjB0MQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMN\n" +
-                "U2FuIEZyYW5jaXNjbzEYMBYGA1UEChMPaGV4dC5zY2Qub3JnLmJyMR4wHAYDVQQD\n" +
-                "ExV0bHNjYS5oZXh0LnNjZC5vcmcuYnIwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNC\n" +
-                "AAQC5bYs8zzRSRNLKaDdGrGZMGvGDWATnB3g9etcWF2AkaHubOMyAukqD5g390SU\n" +
-                "LMvx9jO82GtbOq0J2AHlhel6o20wazAOBgNVHQ8BAf8EBAMCAaYwHQYDVR0lBBYw\n" +
-                "FAYIKwYBBQUHAwIGCCsGAQUFBwMBMA8GA1UdEwEB/wQFMAMBAf8wKQYDVR0OBCIE\n" +
-                "IASnuDit2VEVKTcHnHQoZ22y0Q8X/oIwBmNl+Idm4iWhMAoGCCqGSM49BAMCA0cA\n" +
-                "MEQCIFYGtwRXfxdRVd+8SlMPOgU3ypOdjKFKZ5Y2cdbYrnANAiBagkx+apIfM6B6\n" +
-                "TvR9c3L1PVNAMzYBzPOxek3eoDfV0Q==\n" +
-                "-----END CERTIFICATE-----"}
+                "MIICejCCAiCgAwIBAgIRAKVq2Bmi9xCuXOObXnRRfm8wCgYIKoZIzj0EAwIwgYYx\n" +
+                "CzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYDVQQHEw1TYW4g\n" +
+                "RnJhbmNpc2NvMSEwHwYDVQQKExgwNDM5MTAwNy5oZXh0LnNjZC5vcmcuYnIxJzAl\n" +
+                "BgNVBAMTHnRsc2NhLjA0MzkxMDA3LmhleHQuc2NkLm9yZy5icjAeFw0xOTA1MjAx\n" +
+                "OTQ2MDBaFw0yOTA1MTcxOTQ2MDBaMIGGMQswCQYDVQQGEwJVUzETMBEGA1UECBMK\n" +
+                "Q2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZyYW5jaXNjbzEhMB8GA1UEChMYMDQz\n" +
+                "OTEwMDcuaGV4dC5zY2Qub3JnLmJyMScwJQYDVQQDEx50bHNjYS4wNDM5MTAwNy5o\n" +
+                "ZXh0LnNjZC5vcmcuYnIwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAAST4MDVvLPV\n" +
+                "iFp9bdQBZwiKyHTviI6/d48Dm2/UPC7vOxPFsR/Ll98DAHxY2ET08PiwP0c5SD5W\n" +
+                "UYaesdkryMgro20wazAOBgNVHQ8BAf8EBAMCAaYwHQYDVR0lBBYwFAYIKwYBBQUH\n" +
+                "AwIGCCsGAQUFBwMBMA8GA1UdEwEB/wQFMAMBAf8wKQYDVR0OBCIEILDG9uilqE/f\n" +
+                "VVrBUT46E4yNut5AmQkCVAzcQ49HXz1mMAoGCCqGSM49BAMCA0gAMEUCIQCyBSdu\n" +
+                "MlNdxZvQSfdoBjUtkA0Xy3iQJErhhdJo1cDzAwIgHz4VhX8MOfRPz1FpROttKqqR\n" +
+                "6IwjzVakqiRbqt9wzBg=\n" +
+                "-----END CERTIFICATE-----\n"}
         ],
     transaction: "qualificaDispositivo",
     argumentos:["F93F54D9BF26556C995F8871B142D3376B33CB85825001D08A80891BE4CC338F", "65261DDEF1A6C13C6908C15F93E19B52E1B9A640D6F83302792E28B648B63167", "3EE4FAE0A963278D439B6AFDDEE1B0C258989683DCDC72D10B841D442A4999CB", "1", "", "", "xxxxxxxx", "assinatura", "idCertificado"]
